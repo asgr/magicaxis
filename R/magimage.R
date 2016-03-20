75653b485be66b04079919e7f1978bec5f6fa04d
Aaron Robotham <aaron.robotham@uwa.edu.au>
2016-03-20 10:13:54 +0800

    Added magmap option to magimage.
    Other small fixed.

--- R/magimage.R
+++ R/magimage.R
@@ -1,4 +1,4 @@
-magimage<-function(x, y, z, zlim, xlim, ylim, col = grey((0:1e3)/1e3), add = FALSE, useRaster=TRUE, asp=1, lo=0, hi=1, flip=FALSE, range=c(0,1), type = "quan", stretch="lin", stretchscale=1, bad=NA, clip="", axes=TRUE, frame.plot=TRUE, ...) {
+magimage<-function(x, y, z, zlim, xlim, ylim, col = grey((0:1e3)/1e3), add = FALSE, useRaster=TRUE, asp=1, magmap=FALSE, lo=0, hi=1, flip=FALSE, range=c(0,1), type = "quan", stretch="lin", stretchscale=1, bad=NA, clip="", axes=TRUE, frame.plot=TRUE, ...) {
   if(is.list(x)){
     if('y' %in% names(x)){y=x$y}
     if('z' %in% names(x)){z=x$z}
@@ -20,7 +20,6 @@ magimage<-function(x, y, z, zlim, xlim, ylim, col = grey((0:1e3)/1e3), add = FAL
   }
   if(missing(xlim)){xlim=range(x)}
   if(missing(ylim)){ylim=range(y)}
-  if(missing(zlim)){zlim=range}
   if(x[1]>x[length(x)]){x=rev(x); xlim=rev(xlim)}
   if(y[1]>y[length(y)]){y=rev(y); ylim=rev(ylim)}
   if(length(x)==dim(z)[1]){
@@ -33,7 +32,12 @@ magimage<-function(x, y, z, zlim, xlim, ylim, col = grey((0:1e3)/1e3), add = FAL
     ylim[1]=ylim[1]-oldrange/dim(z)[2]
     ylim[2]=ylim[2]+oldrange/dim(z)[2]
   }
-  z=magmap(data=z, lo=lo, hi=hi, flip=flip, range=range, type=type, stretch=stretch, stretchscale=stretchscale, bad=bad, clip=clip)$map
+  if(magmap){
+    z=magmap(data=z, lo=lo, hi=hi, flip=flip, range=range, type=type, stretch=stretch, stretchscale=stretchscale, bad=bad, clip=clip)$map
+  }
+  if(missing(zlim)){
+    zlim=range(z)
+  }
   image(x=x, y=y, z=z, zlim=zlim, xlim=xlim, ylim=ylim, col=col, add=add, useRaster=useRaster, axes=FALSE, asp=asp, xlab='', ylab='', main='')
   if(add==FALSE){
     if(axes){
