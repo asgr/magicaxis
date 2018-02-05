692b043ec6943975b13913bbb98fc3c30939fe81
Aaron Robotham <aaron.robotham@uwa.edu.au>
2018-02-05 17:56:00 +0800

    Better log plot behaviour- will plot positive part of error bars on log plots.

--- R/magerr.R
+++ R/magerr.R
@@ -17,7 +17,7 @@ if(length(errbarsel)>0){
   	xlodraw[sel]=1e-300
   }
   if(doxhi & par()$xlog){
-    sel=which(xhidraw>0 & x<0)
+    sel=which(xhidraw>0 & x<=0)
     x[sel]=1e-300
   }
   if(doylo & par()$ylog){
@@ -25,7 +25,7 @@ if(length(errbarsel)>0){
   	ylodraw[sel]=1e-300
   }
   if(doyhi & par()$ylog){
-    sel=which(yhidraw>0 & y<0)
+    sel=which(yhidraw>0 & y<=0)
     y[sel]=1e-300
   }
   xarrow=x[errbarsel]
