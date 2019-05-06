1e40ac1c8523ee3f80bba6652572ad57ba18e293
Aaron Robotham <aaron.robotham@uwa.edu.au>
2019-05-06 14:53:01 +0800

    Extra magclip outputs are now optional. Faster if this is set to FALSE.

--- R/magclip.R
+++ R/magclip.R
@@ -1,32 +1,60 @@
-magclip=function(x, sigma='auto', clipiters=5, sigmasel=1, estimate='both'){
-  sel = !is.na(x) & !is.nan(x) & !is.null(x) & is.finite(x)
+magclip=function(x, sigma='auto', clipiters=5, sigmasel=1, estimate='both', extra=TRUE){
+  if(extra){
+    xord=order(x)
+    sel = !is.na(x[xord]) & !is.nan(x[xord]) & !is.null(x[xord]) & is.finite(x[xord])
+    clipx=x[xord][sel]
+  }else{
+    sel = !is.na(x) & !is.nan(x) & !is.null(x) & is.finite(x)
+    clipx=sort(x[sel])
+  }
   if(clipiters>0){
-    newlen=length(x)
-    clipx=x[sel]
+    clipx=sort(x[sel])
+    newlen=length(clipx)
+    sigcut=pnorm(sigmasel)
+    
     for(i in 1:clipiters){
+      if(newlen<=1){break}
       oldlen=newlen
-      roughmed=median(clipx, na.rm=TRUE)
+      roughmed=clipx[newlen/2]
       if(sigma=='auto'){
         clipsigma=qnorm(1-2/max(newlen,2,na.rm=TRUE))
       }else{
         clipsigma=sigma
       }
       if(estimate=='both'){
-        vallims=clipsigma*diff(quantile(clipx,pnorm(c(-sigmasel,sigmasel)), na.rm=TRUE))/2/sigmasel
+        #vallims=clipsigma*diff(quantile(clipx,c(1-sigcut,sigcut)))/2/sigmasel
+        vallims=clipsigma*(clipx[sigcut*newlen]-clipx[(1-sigcut)*newlen])/2/sigmasel
       }
       if(estimate=='lo'){
-        vallims=clipsigma*diff(quantile(clipx,pnorm(c(-sigmasel,0)), na.rm=TRUE))/sigmasel
+        #vallims=clipsigma*(roughmed-quantile(clipx,1-sigcut))/sigmasel
+        vallims=clipsigma*(roughmed-clipx[(1-sigcut)*newlen])/sigmasel
       }
       if(estimate=='hi'){
-        vallims=clipsigma*diff(quantile(clipx,pnorm(c(0,sigmasel)), na.rm=TRUE))/sigmasel
+        #vallims=clipsigma*(quantile(clipx,sigcut)-roughmed)/sigmasel
+        vallims=clipsigma*(clipx[sigcut*newlen]-roughmed)/sigmasel
       }
-      cliplogic=x>=(roughmed-vallims) & x<=(roughmed+vallims) & sel
-      clipx=x[which(cliplogic)]
-      newlen=length(clipx)
-      if(oldlen==newlen | newlen<=1){break}
+      if(extra){
+        cliplogic=x[xord]>=(roughmed-vallims) & x[xord]<=(roughmed+vallims) & sel
+        clipx=x[xord][which(cliplogic)]
+        newlen=length(clipx)
+      }else{
+        clipx=clipx[clipx>=(roughmed-vallims) & clipx<=(roughmed+vallims)]
+        newlen=length(clipx)
+      }
+      if(oldlen==newlen){break}
     }
   }else{
-    cliplogic=rep(TRUE,length(clipx))
+    clipx=x
+    if(extra){
+      cliplogic=TRUE
+    }
   }
-  return=list(x=clipx, clip=cliplogic, range=range(clipx, na.rm = TRUE))
+  if(extra){
+    cliplogic[xord]=cliplogic
+    range=range(clipx, na.rm = TRUE)
+  }else{
+    cliplogic=NA
+    range=NA
+  }
+  invisible(list(x=clipx, clip=cliplogic, range=range))
 }
