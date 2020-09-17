622e4b7ec677af828d19a2630067479cc90386b9
Aaron Robotham <aaron.robotham@uwa.edu.au>
2020-09-17 23:06:42 +0800

    fixes

--- R/maghex.R
+++ R/maghex.R
@@ -3,7 +3,7 @@ hexgrid = function(xlim=c(0,100), ylim=c(0,100), step=1){
   xvec2 = seq(xlim[1]+step/2, xlim[2]-step/2, by=step)
   
   yvec1 = seq(ylim[1], ylim[2], by=step*sqrt(3))
-  yvec2 = seq(ylim[1]+step*sqrt(3)/2, xlim[2]-step*sqrt(3)/2, by=step*sqrt(3))
+  yvec2 = seq(ylim[1]+step*sqrt(3)/2, ylim[2]-step*sqrt(3)/2, by=step*sqrt(3))
   
   grid1 = expand.grid(x=xvec1, y=yvec1)
   grid2 = expand.grid(x=xvec2, y=yvec2)
@@ -26,6 +26,7 @@ hexcount = function(x, y, xlim=NULL, ylim=NULL, step=diff(xlim)/50, k=1e3, dustl
     output = nn2(cbind(x,y), grid, searchtype='radius', radius=1.050075*step/2, k=k) #slightly smaller radius so on average we do not overcount
   }
   output$nn.dists[output$nn.dists>1.34e154]=NA
+  output$nn.idx[output$nn.idx==0] = NA
   if(exacthex){
     overlap = output$nn.idx[output$nn.dists>0.845299*step/2 & output$nn.idx>0]
     findclash = duplicated(overlap, incomparables=0)
@@ -35,18 +36,25 @@ hexcount = function(x, y, xlim=NULL, ylim=NULL, step=diff(xlim)/50, k=1e3, dustl
       #print(i)
       sel = which(output$nn.idx[allclashIDs]==i)
       minsel = which.min(output$nn.dists[allclashIDs][sel])
-      output$nn.idx[allclashIDs][sel[-minsel]] = 0
+      output$nn.idx[allclashIDs][sel[-minsel]] = NA
       #output$nn.dists[sel[-minsel]] = NA
     }
   }
-  hexcounts = k - rowSums(output$nn.idx==0)
+  hexcounts = k - rowSums(is.na(output$nn.idx))
   if(dustlim>0){
     dust = unique(output$nn.idx[hexcounts <= dustlim,])
-    dust = dust[-which(dust==0)]
+    dust = dust[-which(is.na(dust))]
   }else{
     dust=NULL
   }
-  output = list(hexbins=cbind(grid, hexcounts), dust=data.frame(x=x[dust], y=y[dust]), xlim=xlim, ylim=ylim, step=step, dustlim=dustlim)
+  
+  hexrefs = rep.int(1:dim(output$nn.idx)[1],times=k)
+  groups = rep(NA,length(x))
+  replace = which(!is.na(output$nn.idx))
+  groups[output$nn.idx[replace]] = hexrefs[replace]
+  
+  output = list(hexbins=cbind(grid, hexcounts), dust=data.frame(x=x[dust], y=y[dust]),
+                xlim=xlim, ylim=ylim, step=step, dustlim=dustlim, groups=groups)
   class(output) = 'hexcount'
   return(output)
 }
@@ -70,7 +78,7 @@ plot.hexcount = function(x, colramp=terrain.colors(1e4), ...){
 maghex = function(x, y, xlim=NULL, ylim=NULL, step=diff(xlim)/50, k=1e3, dustlim=1, exacthex=FALSE, colramp=terrain.colors(1e4)){
   if(missing(y)){
     if(!is.null(dim(x))){
-      if(dim(x)[2]>=2){y=x[,2];x=x[,1]}
+      if(dim(x)[2]>=2){y=unlist(x[,2]); x=unlist(x[,1])}
     }
   }
   use=!is.na(x) & !is.nan(x) & !is.null(x) & is.finite(x) & !is.na(y) & !is.nan(y) & !is.null(y) & is.finite(y)
