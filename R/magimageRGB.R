947f40f72a0b8b5952df4669fae8dca9e923d98b
Aaron Robotham <aaron.robotham@uwa.edu.au>
2023-02-09 09:50:52 +0800

    Removed deprecated WCS related functions in magicaxis. All of these now exist in the (much better) Rwcs package (GitHub asgr/Rwcs) which uses wcslib for all its coordinate transformations.
    Don't complain! They are not coming back here!!

--- R/magimageRGB.R
+++ R/magimageRGB.R
@@ -1,4 +1,8 @@
-magimageRGB<-function(x, y, R, G, B, saturation=1, zlim, xlim, ylim, add = FALSE, useRaster=TRUE, asp=1, magmap=TRUE, locut=0.4, hicut=0.995, flip=FALSE, range=c(0,1), type = "quan", stretch="asinh", stretchscale='auto', bad=range[1], clip="", axes=TRUE, frame.plot=TRUE, sparse='auto', ...){
+magimageRGB<-function(x, y, R, G, B, saturation=1, zlim, xlim, ylim, add = FALSE,
+                      useRaster=TRUE, asp=1, magmap=TRUE, locut=0.4, hicut=0.995,
+                      flip=FALSE, range=c(0,1), type = "quan", stretch="asinh",
+                      stretchscale='auto', bad=range[1], clip="", axes=TRUE, frame.plot=TRUE,
+                      sparse='auto', ...){
   dots=list(...)
   dotskeepimage=c('xaxs', 'yaxs', 'breaks', 'oldstyle')
   if(length(dots)>0){
@@ -99,123 +103,4 @@ magimageRGB<-function(x, y, R, G, B, saturation=1, zlim, xlim, ylim, add = FALSE
     }
   }
   return=list(x=x, y=y, R=R, G=G, B=B)
-}
-
-magimageWCSRGB=function(R, G, B, header_out, Rheader, Gheader, Bheader, dowarp='auto', direction = "auto", boundary = "dirichlet", interpolation = "cubic", n, grid.col='grey', grid.lty=2, grid.lwd=0.5, lab.col='green', coord.type='sex', margin=TRUE, loc.diff=c(0,0), xlab='Right Ascension', ylab='Declination', mgp=c(2,0.5,0), mtline=2, position='topright', com.col="green", com.length=0.05, coord.axis='auto', pretty='auto', CRVAL1=0, CRVAL2=0, CRPIX1=0, CRPIX2=0, CD1_1=1, CD1_2=0, CD2_1=0, CD2_2=1, CTYPE1='RA--TAN', CTYPE2='DEC--TAN', ...){
-  
-  if(missing(xlab)){
-    if(coord.type=='sex'){
-      xlab=paste(xlab,'/ H:M:S')
-    }
-    if(coord.type=='deg'){
-      xlab=paste(xlab,'/ deg')
-    }
-  }
-  if(missing(ylab)){
-    if(coord.type=='sex'){
-      ylab=paste(ylab,'/ D:M:S')
-    }
-    if(coord.type=='deg'){
-      ylab=paste(ylab,'/ deg')
-    }
-  }
-  
-  if(!missing(R)){
-    if(any(names(R)=='imDat')){
-      if(missing(Rheader)){
-        Rheader=R$hdr
-      }
-      R=R$imDat
-    }
-    if(any(names(R)=='dat')){
-      if(missing(Rheader)){
-        Rheader=R$hdr[[1]]
-        Rheader=data.frame(key=Rheader[,1],value=Rheader[,2], stringsAsFactors = FALSE)
-      }
-      R=R$dat[[1]]
-    }
-    if(any(names(R)=='image')){
-      if(missing(Rheader)){
-        Rheader=R$header
-      }
-      R=R$image
-    }
-  }
-  
-  if(!missing(G)){
-    if(any(names(G)=='imDat')){
-      if(missing(Gheader)){
-        Gheader=G$hdr
-      }
-      G=G$imDat
-    }
-    if(any(names(G)=='dat')){
-      if(missing(Gheader)){
-        Gheader=G$hdr[[1]]
-        Gheader=data.frame(key=Gheader[,1],value=Gheader[,2], stringsAsFactors = FALSE)
-      }
-      G=G$dat[[1]]
-    }
-    if(any(names(G)=='image')){
-      if(missing(Gheader)){
-        Gheader=G$header
-      }
-      G=G$image
-    }
-  }
-  
-  if(!missing(B)){
-    if(any(names(B)=='imDat')){
-      if(missing(Bheader)){
-        Bheader=B$hdr
-      }
-      B=B$imDat
-    }
-    if(any(names(B)=='dat')){
-      if(missing(Bheader)){
-        Bheader=B$hdr[[1]]
-        Bheader=data.frame(key=Bheader[,1],value=Bheader[,2], stringsAsFactors = FALSE)
-      }
-      B=B$dat[[1]]
-    }
-    if(any(names(B)=='image')){
-      if(missing(Bheader)){
-        Bheader=B$header
-      }
-      B=B$image
-    }
-  }
-  
-  if(missing(header_out)){
-    if(exists('Rheader')){
-      header_out=Rheader
-    }else if(exists('Gheader')){
-      header_out=Gheader
-    }else if(exists('Bheader')){
-      header_out=Bheader
-    }
-  }
-  
-  if(dowarp=='auto'){
-    dowarp=FALSE
-    if(all(dim(R)==dim(G))==FALSE){dowarp=TRUE}
-    if(all(dim(R)==dim(B))==FALSE){dowarp=TRUE}
-    if(all(as.character(Rheader)==as.character(Gheader))==FALSE){dowarp=TRUE}
-    if(all(as.character(Rheader)==as.character(Gheader))==FALSE){dowarp=TRUE}
-  }
-  
-  if(dowarp){
-    R=magwarp(image_in=R, header_out=header_out, header_in=Rheader, direction=direction, boundary=boundary, interpolation=interpolation)$image
-    G=magwarp(image_in=G, header_out=header_out, header_in=Gheader, direction=direction, boundary=boundary, interpolation=interpolation)$image
-    B=magwarp(image_in=B, header_out=header_out, header_in=Bheader, direction=direction, boundary=boundary, interpolation=interpolation)$image
-  }
-  
-  output=magimageRGB(R=R, G=G, B=B, axes=FALSE, ...)
-  box()
-  
-  magimageWCSGrid(header=header_out, n=n, grid.col=grid.col, grid.lty=grid.lty, grid.lwd=grid.lwd, coord.type=coord.type, loc.diff=loc.diff, pretty=pretty, CRVAL1=CRVAL1, CRVAL2=CRVAL2, CRPIX1=CRPIX1, CRPIX2=CRPIX2, CD1_1=CD1_1, CD1_2=CD1_2, CD2_1=CD2_1, CD2_2=CD2_2, CTYPE1=CTYPE1, CTYPE2=CTYPE2)
-  magimageWCSLabels(header=header_out, n=n, lab.col=lab.col, coord.type=coord.type, margin=margin, loc.diff=loc.diff, xlab=xlab, ylab=ylab, mgp=mgp, mtline=mtline, pretty=pretty, CRVAL1=CRVAL1, CRVAL2=CRVAL2, CRPIX1=CRPIX1, CRPIX2=CRPIX2, CD1_1=CD1_1, CD1_2=CD1_2, CD2_1=CD2_1, CD2_2=CD2_2, CTYPE1=CTYPE1, CTYPE2=CTYPE2)
-  magimageWCSCompass(header=header_out, position=position, com.col=com.col, com.length=com.length, loc.diff=loc.diff, CRVAL1=CRVAL1, CRVAL2=CRVAL2, CRPIX1=CRPIX1, CRPIX2=CRPIX2, CD1_1=CD1_1, CD1_2=CD1_2, CD2_1=CD2_1, CD2_2=CD2_2, CTYPE1=CTYPE1, CTYPE2=CTYPE2)
-  
-  return=output
 }
